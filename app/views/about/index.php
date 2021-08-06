3bcdfb69435f4d30136231daf87d15ec26313891
febrio-d <7g26febrio@gmail.com>
2021-08-06 11:28:03 +0700

    Add sql database

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,5 @@
 <div class="container-fluid mt-2">
     <h3>About Me</h3>
     <img src="<?= BASEURL; ?>/img/profile.jpg" alt="Febrio" width="200" class="img-thumbnail">
+    <p>My name is Moch. Febrio DA. 17 years old programmer based on Malang, East Java.</p>
 </div>
---
+++ learn-phpmvc.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 06, 2021 at 06:25 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `learn-phpmvc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `cid` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `major` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `name`, `cid`, `email`, `major`) VALUES
+(1, 'Anthony Grant', '00113231', 'grant78@gmail.com', 'Computer Science'),
+(2, 'Janice Anderson', '00098126', 'jnice12@gmail.com', 'Philosophy'),
+(3, 'Jamie Groenen', '00109231', 'jamieg33@gmail.com', 'Data Science');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
